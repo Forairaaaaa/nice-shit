d9c6f568bd9782fdc9be316edad6c483e816a5aa
Forairaaaaa <applesyqd@outlook.com>
2023-12-02 13:44:30 +0800

    rm blob, change pic list into json file reading

--- app/gallery-wall/page.tsx
+++ app/gallery-wall/page.tsx
@@ -1,98 +1,32 @@
+import { promises as fs } from "fs";
 import { FramedPictureProps } from "./components/framed-picture";
 import GalleryWall from "./components/gallery-wall";
 
-const pictureList: FramedPictureProps[] = [
-  {
-    imageSrc:
-      "https://images.unsplash.com/photo-1700943872245-569730a1714b?q=80&w=2670",
-    nameTag: "Nice Shit",
-    timeTag: "2023.12.2",
-  },
-  {
-    imageSrc: "https://images.unsplash.com/photo-1701182811522-aa9a5eb07248",
-    nameTag: "Skate!",
-    timeTag: "2023.12.3",
-  },
-  {
-    imageSrc:
-      "https://images.unsplash.com/photo-1700943872245-569730a1714b?q=80&w=2670",
-    nameTag: "Nice Shit",
-    timeTag: "2023.12.2",
-  },
-  {
-    imageSrc: "https://images.unsplash.com/photo-1701182811522-aa9a5eb07248",
-    nameTag: "Skate!",
-    timeTag: "2023.12.3",
-  },
-  {
-    imageSrc:
-      "https://images.unsplash.com/photo-1700943872245-569730a1714b?q=80&w=2670",
-    nameTag: "Nice Shit",
-    timeTag: "2023.12.2",
-  },
-  {
-    imageSrc: "https://images.unsplash.com/photo-1701182811522-aa9a5eb07248",
-    nameTag: "Skate!",
-    timeTag: "2023.12.3",
-  },
-  {
-    imageSrc:
-      "https://images.unsplash.com/photo-1700943872245-569730a1714b?q=80&w=2670",
-    nameTag: "Nice Shit",
-    timeTag: "2023.12.2",
-  },
-  {
-    imageSrc: "https://images.unsplash.com/photo-1701182811522-aa9a5eb07248",
-    nameTag: "Skate!",
-    timeTag: "2023.12.3",
-  },
-  {
-    imageSrc:
-      "https://images.unsplash.com/photo-1700943872245-569730a1714b?q=80&w=2670",
-    nameTag: "Nice Shit",
-    timeTag: "2023.12.2",
-  },
-  {
-    imageSrc: "https://images.unsplash.com/photo-1701182811522-aa9a5eb07248",
-    nameTag: "Skate!",
-    timeTag: "2023.12.3",
-  },
-  {
-    imageSrc:
-      "https://images.unsplash.com/photo-1700943872245-569730a1714b?q=80&w=2670",
-    nameTag: "Nice Shit",
-    timeTag: "2023.12.2",
-  },
-  {
-    imageSrc: "https://images.unsplash.com/photo-1701182811522-aa9a5eb07248",
-    nameTag: "Skate!",
-    timeTag: "2023.12.3",
-  },
-  {
-    imageSrc:
-      "https://images.unsplash.com/photo-1700943872245-569730a1714b?q=80&w=2670",
-    nameTag: "Nice Shit",
-    timeTag: "2023.12.2",
-  },
-  {
-    imageSrc: "https://images.unsplash.com/photo-1701182811522-aa9a5eb07248",
-    nameTag: "Skate!",
-    timeTag: "2023.12.3",
-  },
-  {
-    imageSrc:
-      "https://images.unsplash.com/photo-1700943872245-569730a1714b?q=80&w=2670",
-    nameTag: "Nice Shit",
-    timeTag: "2023.12.2",
-  },
-];
+async function getPictureListFromFs() {
+  const file = await fs.readFile(
+    process.cwd() + "/public/gallery-wall/gallery-wall-props.json",
+    "utf-8"
+  );
+  const data = JSON.parse(file);
 
-export default function PageGalleryWall() {
+  let pictureList: FramedPictureProps[] = [];
+  (data.pictureProps as FramedPictureProps[]).forEach((props) => {
+    pictureList.push({
+      imageSrc: props.imageSrc,
+      nameTag: props.nameTag,
+      timeTag: props.timeTag,
+    });
+  });
 
+  // console.log(pictureList);
+  return pictureList;
+}
+
+export default async function PageGalleryWall() {
   return (
     <div className="background-project-wall">
       <div className="background-noise-filter">
-        {GalleryWall(pictureList)}
+        {GalleryWall(await getPictureListFromFs())}
       </div>
     </div>
   );
