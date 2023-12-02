9636925655292e4defba55fcd2cae559b0c0c6f6
Forairaaaaa <applesyqd@outlook.com>
2023-12-02 15:08:46 +0800

    add gallery wall config

--- app/gallery-wall/components/gallery-wall.tsx
+++ app/gallery-wall/components/gallery-wall.tsx
@@ -1,24 +1,13 @@
 import FramedPicture, { FramedPictureProps } from "./framed-picture";
 
-export default function GalleryWall(
-  picturePropsList: FramedPictureProps[],
-  randomOrder: boolean = false
-) {
-  const shuffle = (array: FramedPictureProps[]) => {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
-  };
+interface GalleryWallProps {
+  picturePropsList: FramedPictureProps[];
+}
 
-  if (randomOrder) {
-    picturePropsList = shuffle(picturePropsList);
-  }
-
+export default function GalleryWall(props: GalleryWallProps) {
   return (
     <div className="flex flex-wrap justify-between items-center xl:py-20 xl:p-40 gap-x-10 gap-y-20">
-      {picturePropsList.map((props) => (
+      {props.picturePropsList.map((props) => (
         <FramedPicture
           key={props.imageSrc}
           imageSrc={props.imageSrc}
--- app/gallery-wall/page.tsx
+++ app/gallery-wall/page.tsx
@@ -1,32 +1,74 @@
 import { promises as fs } from "fs";
 import { FramedPictureProps } from "./components/framed-picture";
 import GalleryWall from "./components/gallery-wall";
+import styles from "./styles.module.css";
 
-async function getPictureListFromFs() {
+interface GalleryWallConfig {
+  backgroudImage: string;
+  randomOrder: boolean;
+  pictureList: FramedPictureProps[];
+}
+
+async function getGalleryWallConfigFromFs() {
   const file = await fs.readFile(
     process.cwd() + "/public/gallery-wall/gallery-wall-props.json",
     "utf-8"
   );
   const data = JSON.parse(file);
 
-  let pictureList: FramedPictureProps[] = [];
-  (data.pictureProps as FramedPictureProps[]).forEach((props) => {
-    pictureList.push({
+  let result: GalleryWallConfig = {
+    backgroudImage: "",
+    randomOrder: false,
+    pictureList: [],
+  };
+
+  // Background image
+  if (data.backgroudImage != "") {
+    result.backgroudImage = data.backgroudImage;
+  }
+
+  // Random order
+  if (data.randomOrder) {
+    result.randomOrder = data.randomOrder;
+  }
+
+  // Picture list
+  (data.pictureList as FramedPictureProps[]).forEach((props) => {
+    result.pictureList.push({
       imageSrc: props.imageSrc,
       nameTag: props.nameTag,
       timeTag: props.timeTag,
     });
   });
 
-  // console.log(pictureList);
-  return pictureList;
+  // console.log(result);
+  return result;
 }
 
 export default async function PageGalleryWall() {
+  const config = await getGalleryWallConfigFromFs();
+
+  // Shuffle for random order
+  const shuffle = (array: FramedPictureProps[]) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  };
+
+  if (config.randomOrder) {
+    config.pictureList = shuffle(config.pictureList);
+  }
+
   return (
-    <div className="background-project-wall">
-      <div className="background-noise-filter">
-        {GalleryWall(await getPictureListFromFs(), true)}
+    <div
+      className={
+        styles.background + " bg-[url('" + config.backgroudImage + "')]"
+      }
+    >
+      <div className={styles.backgroundNoiseFilter}>
+        <GalleryWall picturePropsList={config.pictureList}></GalleryWall>
       </div>
     </div>
   );
